771eee759d68cc80f9809308c1d77607366a9bba
sgollapudi77 <85578033+sgollapudi77@users.noreply.github.com>
2022-06-01 15:12:56 +0530

    feat: Add Remote images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const data = require("./pages/api/products.json")
 module.exports = {
   trailingSlash: true,
   distDir : 'build',
+  images: {
+    domains: ['images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com'],
+  },
   // target: 'serverless',
   exportPathMap: function(defaultPathMap,{ dev= true, dir, outDir, distDir, buildId }) {
     const paths = {
